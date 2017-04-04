bc2a2ace2a8e2dafab43804e71bffe3e65526305
tridipkolkata <tridip2000@gmail.com>
2017-04-04 19:43:45 +0530

    regular factory added

---
+++ Factory_GenericFactory/FactorySample1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Factory_GenericFactory
+{
+    partial class FactorySample1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUPS = new System.Windows.Forms.Button();
+            this.btnFedEx = new System.Windows.Forms.Button();
+            this.btnPurolator = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnUPS
+            // 
+            this.btnUPS.Location = new System.Drawing.Point(88, 17);
+            this.btnUPS.Name = "btnUPS";
+            this.btnUPS.Size = new System.Drawing.Size(75, 23);
+            this.btnUPS.TabIndex = 0;
+            this.btnUPS.Text = "UPS";
+            this.btnUPS.UseVisualStyleBackColor = true;
+            this.btnUPS.Click += new System.EventHandler(this.btnUPS_Click);
+            // 
+            // btnFedEx
+            // 
+            this.btnFedEx.Location = new System.Drawing.Point(88, 62);
+            this.btnFedEx.Name = "btnFedEx";
+            this.btnFedEx.Size = new System.Drawing.Size(75, 23);
+            this.btnFedEx.TabIndex = 1;
+            this.btnFedEx.Text = "FedEx";
+            this.btnFedEx.UseVisualStyleBackColor = true;
+            this.btnFedEx.Click += new System.EventHandler(this.btnFedEx_Click);
+            // 
+            // btnPurolator
+            // 
+            this.btnPurolator.Location = new System.Drawing.Point(88, 107);
+            this.btnPurolator.Name = "btnPurolator";
+            this.btnPurolator.Size = new System.Drawing.Size(75, 23);
+            this.btnPurolator.TabIndex = 2;
+            this.btnPurolator.Text = "Purolator";
+            this.btnPurolator.UseVisualStyleBackColor = true;
+            this.btnPurolator.Click += new System.EventHandler(this.btnPurolator_Click);
+            // 
+            // FactorySample1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 159);
+            this.Controls.Add(this.btnPurolator);
+            this.Controls.Add(this.btnFedEx);
+            this.Controls.Add(this.btnUPS);
+            this.Name = "FactorySample1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUPS;
+        private System.Windows.Forms.Button btnFedEx;
+        private System.Windows.Forms.Button btnPurolator;
+    }
+}
+
---
+++ Factory_GenericFactory/FactorySample1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factory_GenericFactory
+{
+    public partial class FactorySample1 : Form
+    {
+        public FactorySample1()
+        {
+            InitializeComponent();
+        }
+
+        IShip objActivity = null;
+
+
+        private void btnUPS_Click(object sender, EventArgs e)
+        {
+            objActivity = ShipperFactory.CreateInstance(Shipper.UPS);
+            objActivity.Ship();
+        }
+
+        private void btnFedEx_Click(object sender, EventArgs e)
+        {
+            objActivity = ShipperFactory.CreateInstance(Shipper.FedEx);
+            objActivity.Ship();
+        }
+
+        private void btnPurolator_Click(object sender, EventArgs e)
+        {
+            objActivity = ShipperFactory.CreateInstance(Shipper.Purolator);
+            objActivity.Ship();
+        }
+    }
+
+    public enum Shipper
+    {
+        UPS = 1,
+        FedEx = 2,
+        Purolator = 3
+    }
+
+    public interface IShip
+    {
+        void Ship();
+    }
+
+    public class ShipperPurolator : IShip
+    {
+        public void Ship()
+        {
+            //-- code logic to implement shipping method for Purolator
+            MessageBox.Show("Purolator ship start");
+        }
+    }
+
+    public class ShipperUPS : IShip
+    {
+        public void Ship()
+        {
+            //-- code logic to implement shipping method for Purolator
+            MessageBox.Show("UPS ship start");
+        }
+    }
+
+    public class ShipperFexEx : IShip
+    {
+        public void Ship()
+        {
+            //-- code logic to implement shipping method for Purolator
+            MessageBox.Show("FedEx ship start");
+        }
+    }
+
+    public class ShipperFactory
+    {
+        public static IShip CreateInstance(Shipper enumModuleName)
+        {
+            IShip objActivity = null;
+
+            switch (enumModuleName)
+            {
+                case Shipper.UPS:
+                    objActivity = new ShipperUPS();
+                    break;
+                case Shipper.FedEx:
+                    objActivity = new ShipperFexEx();
+                    break;
+                case Shipper.Purolator:
+                    objActivity = new ShipperPurolator();
+                    break;
+                default:
+                    break;
+            }
+            return objActivity;
+        }
+    }
+}
